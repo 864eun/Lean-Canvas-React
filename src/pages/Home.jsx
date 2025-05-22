7758fb1854b5a2ae32e697cfd63c33825d0ba262
eun <eun00864@gmail.com>
2025-05-22 15:45:26 +0900

    Add:Loading, Error 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,27 @@ import SearchBar from '../components/TailwindCss/SearchBar';
 import ViewToggle from '../components/TailwindCss/ViewToggle';
 import { useEffect } from 'react';
 import { getCanvases } from '../api/canvas';
+import Loading from '../components/TailwindCss/Loading';
+import Error from '../components/TailwindCss/Error';
 
 function Home() {
   const [searchText, setSearchText] = useState();
   const [isGridView, setIsGridView] = useState(true);
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   async function fetchData(params) {
-    const response = await getCanvases(params);
-    setData(response.data);
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await getCanvases(params);
+      setData(response.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -29,12 +41,21 @@ function Home() {
         <SearchBar searchText={searchText} setSearchText={setSearchText} />
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
-      <CanvasList
-        filteredData={data}
-        isGridView={isGridView}
-        searchText={searchText}
-        onDeleteItem={handleDeleteItem}
-      />
+      {isLoading && <Loading />}
+      {error && (
+        <Error
+          message={error.message}
+          onRetry={() => fetchData({ title_like: searchText })}
+        />
+      )}
+      {!isLoading && !error && (
+        <CanvasList
+          filteredData={data}
+          isGridView={isGridView}
+          searchText={searchText}
+          onDeleteItem={handleDeleteItem}
+        />
+      )}
     </div>
   );
 }
