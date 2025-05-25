0c8ab8e15c66f0da6b9759a4bb05b1c9e9c48143
eun <eun00864@gmail.com>
2025-05-25 18:16:48 +0900

    Add: React Query 최적화

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import './index.css';
 import ErrorPage from './pages/ErrorPage.jsx';
 import CanvasDetail from './pages/CanvasDetail.jsx';
 import Home from './pages/Home.jsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
@@ -41,6 +42,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
